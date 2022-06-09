e0802914c51f077d0c177bf822c22ee95b47818e
nghair <angelina@haar.dev>
2022-06-09 16:31:13 +0200

    pushy fixy

--- src/assets/products.ts
+++ src/assets/products.ts
@@ -1,10 +1,11 @@
 export const products = [
   {
     name: 'Das große Yoga Vidya Hatha Yoga Buch',
+    kategorie: 'Yoga Vidya Verlag:Bücher',
     hersteller: 'Yoga Vidya Verlag',
     artikelNr: 100057,
     ean: 9783943376166,
     gewicht: 0.81,
-    preis: 19,90
+    preis: 19.9,
   },
 ];
